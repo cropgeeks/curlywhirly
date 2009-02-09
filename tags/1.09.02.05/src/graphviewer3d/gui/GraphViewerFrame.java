77fd86b9ea0eea3183c9258ed5444059a740151e
imilne <imilne@8a050542-6524-4f2e-89c5-c4b64c48d4a8>
2009-02-09 08:45:21 +0000

    - NEW: Tagged public release of 1.09.02.05

---
+++ tags/1.09.02.05/src/graphviewer3d/gui/GraphViewerFrame.java
@@ -0,0 +1,206 @@
+package graphviewer3d.gui;
+
+import graphviewer3d.controller.*;
+import graphviewer3d.data.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.swing.*;
+
+import apple.dts.samplecode.osxadapter.*;
+
+import scri.commons.gui.*;
+
+public class GraphViewerFrame extends JFrame
+{
+
+	// ===================================================vars =================================================
+
+	public FatController controller = new FatController(this);
+	public static DataSet dataSet;
+	public static GraphViewer3DCanvas canvas3D;
+	public static JPanel canvasPanel;
+	public int controlPanelWidth = 200;
+	public MTControlPanel controlPanel;
+	JLabel openLabel;
+	static JCheckBox instructionsCheckBox;
+	public boolean dataLoaded = false;
+
+	private static File prefsFile = new File(System.getProperty("user.home"), ".curlywhirly.xml");
+	public static Preferences prefs = new Preferences();
+	public StatusBar statusBar;
+	public GraphViewerMenuBar menuBar;
+
+	public static void main(String[] args)
+	{
+		// OS X: This has to be set before anything else
+		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "CurlyWhirly");
+
+		try
+		{
+			// preferences
+			prefs.loadPreferences(prefsFile, Preferences.class);
+
+			// Set System L&F
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		new GraphViewerFrame();
+	}
+
+	GraphViewerFrame()
+	{
+		if (SystemUtils.isMacOS())
+			handleOSXStupidities();
+
+		Install4j.doStartUpCheck();
+
+		setupComponents();
+
+		// get the GUI set up
+		setTitle("CurlyWhirly - " + Install4j.VERSION);
+		setIconImage(new ImageIcon("res/curlywurly_icon32px.png").getImage());
+		pack();
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				shutdown();
+			}
+		});
+
+		setVisible(true);
+	}
+
+	// -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	void shutdown()
+	{
+		prefs.savePreferences(prefsFile, Preferences.class);
+		System.exit(0);
+	}
+
+	// -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	public void loadData(File file)
+	{
+		//load the data from file
+		DataLoader loader = new DataLoader(this);
+		try
+		{
+			dataSet = loader.getDataFromFile(file);
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+
+		// only add the canvas to the panel if we haven't got data loaded already
+		if (!dataLoaded)
+			canvasPanel.add(canvas3D, BorderLayout.CENTER);
+
+		//set up the new dataset and make a new scene graph
+		//normalize first
+		//this sets the data up so that each axis is normalized to between -1 and 1 and the data fills the whole range
+		DataNormalizer.normalizeDataSet(dataSet);
+		canvas3D.dataSet = dataSet;
+		canvas3D.createSceneGraph();
+
+		//do the rest of the set up
+		controlPanel.setUpListData();
+		controlPanel.resetComboBoxes();
+		dataLoaded = true;
+		statusBar.setDefaultText();
+		repaint();
+	}
+
+	// -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	private void setupComponents()
+	{
+		//workaround for the 3D drawing problem with Swing menus
+//		JPopupMenu.setDefaultLightWeightPopupEnabled( false );
+
+		// make a new panel for the 3D panel
+		canvasPanel = new JPanel(new BorderLayout());
+		canvasPanel.setPreferredSize(new Dimension(600, 600));
+
+		// instantiate the canvas here rather than in the data load method
+		// we want to be able to recycle it when we load another dataset over the top of the current one
+		canvas3D = new GraphViewer3DCanvas(this);
+
+		// //add a label instructing the user to open a file
+		openLabel = new JLabel("Open a data file to begin.",JLabel.CENTER);
+		openLabel.setFont(new Font("SANS_SERIF", Font.PLAIN, 18));
+		canvasPanel.add(openLabel, BorderLayout.CENTER);
+		canvasPanel.setBackground(Color.LIGHT_GRAY);
+		openLabel.setForeground(new Color(120,120,120));
+
+		// side panel
+		controlPanel = new MTControlPanel(this);
+
+		// main panel
+		JPanel mainPanel = new JPanel(new BorderLayout());
+		mainPanel.add(controlPanel, BorderLayout.WEST);
+		mainPanel.add(canvasPanel, BorderLayout.CENTER);
+		this.getContentPane().add(mainPanel);
+
+		// menu bar
+		menuBar = new GraphViewerMenuBar(this);
+		this.setJMenuBar(menuBar);
+
+		// status bar
+		statusBar = new StatusBar();
+		getContentPane().add(statusBar, java.awt.BorderLayout.SOUTH);
+
+	}
+
+	// -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	// --------------------------------------------------
+	// Methods required for better native support on OS X
+
+	private void handleOSXStupidities()
+	{
+		try
+		{
+			// Register handlers to deal with the System menu about/quit options
+//			OSXAdapter.setPreferencesHandler(this,
+//				getClass().getDeclaredMethod("osxPreferences", (Class[])null));
+			OSXAdapter.setAboutHandler(this,
+				getClass().getDeclaredMethod("osxAbout", (Class[])null));
+			OSXAdapter.setQuitHandler(this,
+				getClass().getDeclaredMethod("osxShutdown", (Class[])null));
+
+			// Dock the menu bar at the top of the screen
+			System.setProperty("apple.laf.useScreenMenuBar", "true");
+		}
+		catch (Exception e) {}
+	}
+
+	/** "Preferences" on the OS X system menu. */
+	public void osxPreferences()
+	{
+	}
+
+	/** "About CurlyWhirly" on the OS X system menu. */
+	public void osxAbout()
+	{
+		new AboutDialog(this, true);
+	}
+
+	/** "Quit CurlyWhirly" on the OS X system menu. */
+	public boolean osxShutdown()
+	{
+		shutdown();
+		return true;
+	}
+
+}// end class
---
+++ tags/1.09.02.05/src/graphviewer3d/gui/GraphViewerMenuBar.java
@@ -0,0 +1,180 @@
+package graphviewer3d.gui;
+
+import graphviewer3d.controller.ScreenCaptureThread;
+import graphviewer3d.data.FileLoader;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.swing.JFileChooser;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.KeyStroke;
+
+import scri.commons.gui.*;
+
+public class GraphViewerMenuBar extends JMenuBar implements ActionListener
+{
+	// Returns value for "CTRL" under most OSs, and the "apple" key for OS X
+	private int menuShortcut = Toolkit.getDefaultToolkit().getMenuShortcutKeyMask();
+
+	JMenuItem openFileItem;
+	JMenuItem aboutItem;
+	JMenuItem helpItem;
+	JMenuItem helpCtrlItem;
+	JMenuItem exampleDataItem;
+	JMenuItem exitItem;
+	JMenuItem saveItem;
+	GraphViewerFrame frame;
+	JFileChooser fc;
+	DataLoadingDialog dataLoadingDialog;
+
+	public GraphViewerMenuBar(GraphViewerFrame frame)
+	{
+		this.frame = frame;
+		init();
+	}
+
+	private void init()
+	{
+		// this enables swing components to be drawn on top of the 3D canvas
+		JPopupMenu.setDefaultLightWeightPopupEnabled(false);
+
+		// //////////////////////////////////////
+		// the File Menu
+		JMenu fileMenu = new JMenu("File");
+		fileMenu.setMnemonic(KeyEvent.VK_F);
+		this.add(fileMenu);
+
+		// the Open File item
+		openFileItem = new JMenuItem("Open data...");
+		openFileItem.addActionListener(this);
+		openFileItem.setMnemonic(KeyEvent.VK_O);
+		openFileItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, menuShortcut));
+		fileMenu.add(openFileItem);
+
+		// // the example data import item
+		exampleDataItem = new JMenuItem("Load example data");
+		exampleDataItem.setMnemonic(KeyEvent.VK_L);
+		exampleDataItem.addActionListener(this);
+		fileMenu.add(exampleDataItem);
+
+		// separator
+		fileMenu.addSeparator();
+
+		// the save view item
+		saveItem = new JMenuItem("Capture view screenshot");
+		saveItem.setMnemonic(KeyEvent.VK_C);
+		saveItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, menuShortcut));
+		saveItem.addActionListener(this);
+		fileMenu.add(saveItem);
+
+		// separator
+		fileMenu.addSeparator();
+
+		// the Exit item
+		exitItem = new JMenuItem("Exit");
+		exitItem.setMnemonic(KeyEvent.VK_X);
+		exitItem.addActionListener(this);
+		// We don't add this option to OS X as it is auto-added by Apple
+		if (SystemUtils.isMacOS() == false)
+			fileMenu.add(exitItem);
+
+		// ////////////////////////////////////////////////////////////////////////////////
+		// the Help Menu
+		JMenu helpMenu = new JMenu("Help");
+		helpMenu.setMnemonic(KeyEvent.VK_H);
+		this.add(helpMenu);
+
+		// the help item
+		helpItem = new JMenuItem("Online help");
+		helpItem.addActionListener(this);
+		helpItem.setMnemonic(KeyEvent.VK_O);
+		helpItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0));
+		helpMenu.add(helpItem);
+
+		helpCtrlItem = new JMenuItem("Controls summary");
+		helpCtrlItem.addActionListener(this);
+		helpCtrlItem.setMnemonic(KeyEvent.VK_C);
+		helpMenu.add(helpCtrlItem);
+
+		// separator
+		helpMenu.addSeparator();
+
+		// the about item
+		aboutItem = new JMenuItem("About CurlyWhirly");
+		aboutItem.setMnemonic(KeyEvent.VK_A);
+		aboutItem.addActionListener(this);
+		// We don't add this option to OS X as it is auto-added by Apple
+		if (SystemUtils.isMacOS() == false)
+			helpMenu.add(aboutItem);
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		Object src = e.getSource();
+
+		if (src.equals(openFileItem))
+		{
+			// file chooser
+			fc = new JFileChooser(Preferences.lastDir);
+
+			int returnVal = fc.showOpenDialog(frame);
+			if (returnVal == JFileChooser.APPROVE_OPTION)
+			{
+				Preferences.lastDir = "" + fc.getSelectedFile().getParent();
+
+				frame.controller.loadDataInThread(fc.getSelectedFile());
+			}
+		}
+
+		else if (src.equals(exampleDataItem))
+		{
+			// load the example dataset provided with the application
+			frame.controller.loadDataInThread(new File("data/randomData.txt"));
+		}
+
+		else if (src.equals(saveItem))
+		{
+			//save the canvas to this file
+			new ScreenCaptureThread(new File(System.getProperty("user.dir")+System.getProperty("file.separator") +
+							"curlywhirly_screenshot.png"),frame,"png",fc).start();
+		}
+
+		else if (src.equals(exitItem))
+		{
+			frame.shutdown();
+		}
+
+
+		else if (src.equals(helpItem))
+		{
+			String url = "http://bioinf.scri.sari.ac.uk/curlywhirly/manual.shtml";
+
+			GUIUtils.visitURL(url);
+		}
+
+		else if (src.equals(helpCtrlItem))
+		{
+			Instructions3D instr = new Instructions3D(frame);
+			instr.show3DInstructions(false);
+		}
+
+		else if (src.equals(aboutItem))
+		{
+			new AboutDialog(frame, true);
+		}
+
+	}
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+}
