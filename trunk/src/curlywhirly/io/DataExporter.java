7804db66cd9fed186b325121eb257114773bc7c2
gsteph <gsteph@8a050542-6524-4f2e-89c5-c4b64c48d4a8>
2014-04-29 15:12:58 +0000

    - BUG: Datasets without DBurls exported incorrectly.

--- trunk/src/curlywhirly/io/DataExporter.java
+++ trunk/src/curlywhirly/io/DataExporter.java
@@ -12,10 +12,10 @@ public class DataExporter extends SimpleJob
 	private static final String CATEGORY_IDENTIFIER = "categories:";
 	private static final String MISSING_CATEGORY = "Uncategorised";
 	private static final String URL_IDENTIFIER = "#url=";
-    
+
     private final DataSet dataSet;
     private final File outputFile;
-    
+
     public DataExporter(DataSet dataSet, File outputFile)
     {
         this.dataSet = dataSet;
@@ -23,19 +23,20 @@ public class DataExporter extends SimpleJob
     }
 
     @Override
-    public void runJob(int jobId) throws Exception 
-    {   
+    public void runJob(int jobId) throws Exception
+    {
+		System.out.println("Exporting data");
         try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(outputFile))))
         {
             // Output a db URL for point lookup if there is one
             String dbUrl = dataSet.getDbAssociation().getDbPointUrl();
-            if (dbUrl.isEmpty() == false)
+            if (dbUrl != null && dbUrl.isEmpty() == false)
                 writer.println(URL_IDENTIFIER + dbUrl);
-            
+
             // Output the header line
             String header = getHeaderString();
             writer.println(header);
-            
+
             // Get the data for each point and write it out to file
             for (DataPoint dataPoint : dataSet)
             {
@@ -45,28 +46,28 @@ public class DataExporter extends SimpleJob
         }
         catch (Exception e) { e.printStackTrace(); }
     }
-    
-    private String getHeaderString() 
+
+    private String getHeaderString()
     {
         StringBuilder header = new StringBuilder();
-        
+
         // Add categories to the header
         for (CategoryGroup group : dataSet.getCategoryGroups())
             header.append(CATEGORY_IDENTIFIER).append(group.getName()).append("\t");
-        
+
         header.append(LABEL_IDENTIFIER).append("\t");
-        
+
         // Add axis labels to the header
         for (String label : dataSet.getAxisLabels())
             header.append(label).append("\t");
-        
+
         return header.toString().trim();
     }
 
-    private String getDataPointString(DataPoint dataPoint) 
+    private String getDataPointString(DataPoint dataPoint)
     {
         StringBuilder point = new StringBuilder();
-        
+
         // Output the categories assocaited with this data point (in category
         // group order, hence the loop over the dataset's category groups)
         for (CategoryGroup group : dataSet.getCategoryGroups())
@@ -77,12 +78,12 @@ public class DataExporter extends SimpleJob
                 point.append(category.getName());
             point.append("\t");
         }
-        
+
         // Add the name, followed by the original (non-normalized) datapoint values
         point.append(dataPoint.getName()).append("\t");
         for (Float value : dataPoint.getValues())
             point.append(value).append("\t");
-        
+
         return point.toString().trim();
     }
 }
