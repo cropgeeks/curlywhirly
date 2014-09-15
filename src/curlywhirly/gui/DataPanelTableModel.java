6c8ff4157846f54fb12059b4a4aac629baba9a1d
Gordon Stephen <12711372+gordonstephen@users.noreply.github.com>
2014-09-15 12:20:44 +0000

    - NEW: The panel of points in the second tab now displays the list of points sorted firstly by selected state, then by colour.

--- src/curlywhirly/gui/DataPanelTableModel.java
+++ src/curlywhirly/gui/DataPanelTableModel.java
@@ -78,7 +78,7 @@ public class DataPanelTableModel extends AbstractTableModel
 		// can pull out the correct data for display in the table.
 		switch (columnIndex)
 		{
-			case 0: return point.getColor(dataSet.getCurrentCategoryGroup());
+			case 0: return point;
 			case 1: return point;
 			case 2: return point.getValues().get(dataSet.getCurrX());
 			case 3: return point.getValues().get(dataSet.getCurrY());
@@ -98,7 +98,7 @@ public class DataPanelTableModel extends AbstractTableModel
 		return count;
 	}
 
-	static class ColorListRenderer extends DefaultTableCellRenderer
+	class ColorListRenderer extends DefaultTableCellRenderer
 	{
 		// Set the attributes of the class and return a reference
 		@Override
@@ -108,7 +108,8 @@ public class DataPanelTableModel extends AbstractTableModel
 			super.getTableCellRendererComponent(table, value, isSelected,
 				hasFocus, row, column);
 
-			Color color = (Color) value;
+			DataPoint point = (DataPoint) value;
+			Color color = point.getColor(dataSet.getCurrentCategoryGroup());
 
 			// Set the icon
 			BufferedImage image = new BufferedImage(20, 10, BufferedImage.TYPE_INT_RGB);
@@ -165,12 +166,64 @@ public class DataPanelTableModel extends AbstractTableModel
 		}
 	}
 
-	static TableCellRenderer getCellRenderer(int col)
+	TableCellRenderer getCellRenderer(int col)
 	{
 		switch (col)
 		{
 			case 0: return new ColorListRenderer();
 			default: return new PointSelectedRenderer();
+		}
+	}
+
+	DataTableRowSorter getComparator()
+	{
+		return new DataTableRowSorter();
+	}
+
+	class DataTableRowSorter implements Comparator<DataPoint>
+	{
+		@Override
+		public int compare(DataPoint p1, DataPoint p2)
+		{
+			Color color1 = p1.getColor(dataSet.getCurrentCategoryGroup());
+			Color color2 = p2.getColor(dataSet.getCurrentCategoryGroup());
+
+			if (p1.isSelected() && !p2.isSelected())
+			{
+				return 1;
+			}
+			else if (p1.isSelected() && p2.isSelected())
+			{
+				return compare(color1, color2);
+			}
+			else
+				return -1;
+		}
+
+		public int compare(Color c1, Color c2)
+		{
+			if (c1.getAlpha() != c2.getAlpha())
+				return (c2.getAlpha() - c1.getAlpha());
+			// Extract the HSB, and impose the ordering.
+			float[] firstHSB = Color.RGBtoHSB(c1.getRed(),
+							  c1.getGreen(),
+							  c1.getBlue(), null);
+			float[] secondHSB = Color.RGBtoHSB(c2.getRed(),
+							   c2.getGreen(),
+							   c2.getBlue(), null);
+			int[] comp = new int[3];
+			// First saturation...
+			comp[0] = -Float.compare(firstHSB[1], secondHSB[1]);
+			// Then brightness...
+			comp[1] = -Float.compare(firstHSB[2], secondHSB[2]);
+			// Then hue...
+			comp[2] = Float.compare(firstHSB[0], secondHSB[0]);
+
+			// Run through the comparisons, return if not zero.
+			for (int i=0; i<3; i++)
+				if (comp[i] != 0)
+				return comp[i];
+			return 0;
 		}
 	}
 }
