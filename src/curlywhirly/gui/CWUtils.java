13872281a323189967cb13ec15effe3931a58e07
Iain Milne <imilne@users.noreply.github.com>
2015-04-14 08:42:50 +0000

    - NEW: Removed CW's reliance on its local dialog/button panel code in favour of using what's in scri-commons.

--- src/curlywhirly/gui/CWUtils.java
+++ src/curlywhirly/gui/CWUtils.java
@@ -10,17 +10,6 @@ import javax.swing.table.*;
 
 public class CWUtils
 {
-	public static JPanel getButtonPanel()
-	{
-		JPanel p1 = new JPanel(new FlowLayout(FlowLayout.RIGHT, 5, 0));
-
-		p1.setBorder(BorderFactory.createCompoundBorder(
-			BorderFactory.createMatteBorder(1, 0, 0, 0, new Color(219, 219, 219)),
-			BorderFactory.createEmptyBorder(10, 0, 10, 5)));
-
-		return p1;
-	}
-
 	public static void copyTableToClipboard(JTable table, AbstractTableModel model)
 	{
 		StringBuilder text = new StringBuilder();
